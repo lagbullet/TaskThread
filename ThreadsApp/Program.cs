b4e77f0947ba1aa1612ab4fc461c2b48f581906f
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-10-16 14:43:42 +0300

    1

---
+++ ThreadsApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadsApp
+{
+
+    class Program
+    {
+        public static void ThreadMethod(ref bool stopped)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (!stopped) ;
+            sw.Stop();
+            Console.WriteLine($"{sw.Elapsed}");
+        }
+
+    static void Main(string[] args)
+        {
+            bool stopped = false;
+            Thread t = new Thread(() => ThreadMethod(ref stopped))
+            {
+                IsBackground = true
+            };
+            t.Start();
+            Console.ReadLine();
+            stopped = true;
+            Console.ReadLine();
+            Console.WriteLine($"{Console.ReadLine()}");
+
+        }
+    }
+}
